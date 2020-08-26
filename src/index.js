af30cc983d242a6ed02696843ddff83b39294366
Shabi <arber.shabani@hotmail.com>
2020-08-26 11:30:18 +0200

    Update Exports

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import { useAPI } from './useAPI'
 import { useFetchAPI } from './useFetchAPI'
 import { usePaginationAPI } from './usePaginationAPI'
 import { useSearch } from './useSearch'
+import { useCache } from './useCache'
 
-export { useAPI, useFetchAPI, usePaginationAPI, useSearch }
+export { useAPI, useFetchAPI, usePaginationAPI, useSearch, useCache }
